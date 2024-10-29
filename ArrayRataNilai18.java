09c39cb80166d68c053b1909205648f93e4213ef
izamiktsr <mazibackup2@gmail.com>
2024-10-29 23:13:50 +0700

    new

--- ArrayRataNilai18.java
+++ ArrayRataNilai18.java
@@ -7,7 +7,7 @@ public class ArrayRataNilai18 {
         int[] nilaiMhs= new int[10];
         double total = 0;
         double rata2;
-
+    
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = sc.nextInt();
@@ -15,7 +15,13 @@ public class ArrayRataNilai18 {
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
     }
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            if (nilaiMhs[i] > 70) { 
+
+        System.out.println("Mahasiswa ke-" + i + " lulus! " + nilaiMhs[i]);
         rata2 = total/nilaiMhs.length;
         System.out.println("Rata-rata nilai = "+rata2);
 }
+}
+    }
 }
